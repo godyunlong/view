94f27654077ce8ac11f8c64909699ffb489331d7
Ajin <1034693671@qq.com>
2021-01-23 12:52:18 +0800

    添加QQ消息滑动移除View

--- viewlib/src/main/java/com/xy/viewlib/qq/MessageRecyclerView.kt
+++ viewlib/src/main/java/com/xy/viewlib/qq/MessageRecyclerView.kt
@@ -8,11 +8,17 @@ import androidx.recyclerview.widget.RecyclerView
 class MessageRecyclerView(context: Context, attributeSet: AttributeSet?=null)
     :RecyclerView(context,attributeSet) ,MessageTouchListener{
     private var isDown = false
+    private var listener :MessageTouchListener?=null
     override fun onInterceptTouchEvent(e: MotionEvent?): Boolean {
         return !isDown && super.onInterceptTouchEvent(e)
     }
 
     override fun onTouchStatus(isDown:Boolean) {
+        listener?.onTouchStatus(isDown)
         this.isDown = isDown
+    }
+
+    fun bindMessageTouchListener(listener: MessageTouchListener){
+        this.listener = listener
     }
 }
